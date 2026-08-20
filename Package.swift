2f3b6edd1138487f979c8208b8aa3efea8c091e9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:14:56 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-rfc-9111",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
     ],
     products: [
         .library(
@@ -24,12 +24,12 @@ let package = Package(
             name: "RFC 9111",
             dependencies: [
                 .product(name: "RFC 9110", package: "swift-rfc-9110")
-    ]
+            ]
         ),
         .testTarget(
             name: "RFC 9111 Tests",
             dependencies: [
-                "RFC 9111",
+                "RFC 9111"
             ]
         ),
     ],
